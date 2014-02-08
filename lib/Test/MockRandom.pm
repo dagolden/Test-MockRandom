985c2507bf768390a33df9f75afe72553b62a077
David Golden <dagolden@cpan.org>
2014-02-08 05:44:16 -0500

    remove pod boilerplate

--- lib/Test/MockRandom.pm
+++ lib/Test/MockRandom.pm
@@ -488,42 +488,10 @@ compile cycle and should usually be called from within a BEGIN block.  See
 
 Most users will not need this function.  
 
-= BUGS
-
-Please report any bugs or feature requests using the CPAN Request Tracker.  
-Bugs can be submitted through the web interface at 
-[http://rt.cpan.org/Dist/Display.html?Queue=Test::MockRandom]
-
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
-
 = SEE ALSO
 
-* Test::MockObject
-* Test::MockModule
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2004-2007 by David A. Golden
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at 
-[http://www.apache.org/licenses/LICENSE-2.0]
-
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+* [Test::MockObject]
+* [Test::MockModule]
 
 =end wikidoc
 
